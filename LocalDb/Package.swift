ac5ea2a571ed41d1b0b7496dc830eabd6c7049a0
skyaintnolimit <71957845+hanasady@users.noreply.github.com>
2024-11-08 20:07:10 +0300

    add DbContext

---
+++ LocalDb/Package.swift
@@ -0,0 +1,30 @@
+// swift-tools-version: 6.0
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "LocalDb",
+    products: [
+        // Products define the executables and libraries a package produces, making them visible to other packages.
+        .library(
+            name: "LocalDb",
+            targets: ["LocalDb"]),
+    ],
+    dependencies: [
+        .package(url: "https://github.com/realm/realm-swift" , from: "10.54.1"),
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package, defining a module or a test suite.
+        // Targets can depend on other targets in this package and products from dependencies.
+        .target(
+            name: "LocalDb",
+            dependencies: [
+                .product(name: "RealmSwift", package: "realm-swift")
+            ]),
+        .testTarget(
+            name: "LocalDbTests",
+            dependencies: ["LocalDb"]
+        ),
+    ]
+)
